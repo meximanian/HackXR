2e26e97193efb312a617e8d0c5d09bd67e2d03a3
Anthony <aostia@ucsd.edu>
2019-05-04 08:00:21 -0700

    Worked on arm mechanic

---
+++ Fight Club/Assets/Scripts/Armmovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armmovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            rb.velocity = transform.right * speed;
+            Debug.Log("Button us");
+        }
+    }
+}
---
+++ Fight Club/Assets/Scripts/Collision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+
+}
