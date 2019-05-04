7a348b3787b3064ad33a97276a879e0fd07c9f26
Giovanni <g3aguirr@ucsd.edu>
2019-05-04 15:38:20 -0700

    Yifan and Gio's combined push
    has everything Yifan and Gio has worked on up until 3:00pm

---
+++ New Unity Project (2)/Assets/obstacleGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Random;
+
+public class obstacleGenerator : MonoBehaviour
+{
+    int randomNum;
+    GameObject obstacle;
+    GameObject enemy;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            randomNum = Random.Range(0, 5);
+            
+            //if (randomNum == 0 || randomNum == 1)
+            //{
+            //    GameObject prefab = Resources.Load("LeftWall") as GameObject;
+            //    obstacle = Instantiate(prefab) as GameObject;
+            //    obstacle.transform.position = new Vector3(3.0f, 0.07f, i * 20);
+            //}
+            //else if (randomNum == 2 || randomNum == 3)
+            //{
+            //    GameObject prefab = Resources.Load("RightWall") as GameObject;
+            //    obstacle = Instantiate(prefab) as GameObject;
+            //    obstacle.transform.position = new Vector3(6.3f, 0.07f, i * 20);
+            //}
+            if (true)
+            {
+               GameObject prefab = Resources.Load("fullWall") as GameObject;
+               obstacle = Instantiate(prefab) as GameObject;
+                obstacle.transform.position = new Vector3(3.19f, 0.07f, i * 20);
+
+                GameObject prefab2 = Resources.Load("red") as GameObject;
+                enemy = Instantiate(prefab2) as GameObject;
+                enemy.transform.position = new Vector3(0,0, (i * 20) + 10);
+            }
+            //else if (randomNum == 3)
+            //{
+            //    GameObject prefab = Resources.Load("target") as GameObject;
+            //    obstacle = Instantiate(prefab) as GameObject;
+            //    obstacle.transform.position = new Vector3(0, 1.6f, -90 + i * 7);
+            //}
+        }
+
+        //GameObject player = GameObject.Find("Player");
+        //player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+    }
+}
---
+++ New Unity Project (2)/Assets/playerCollider.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int hitCounter = 0;
+
+    public Transform control;
+    public GameObject player;
+    Vector3 forward;
+    Vector3 controllerfwd;
+
+    // Update is called once per frame
+
+    void Start()
+    {
+         forward = player.transform.forward;
+           
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controllerfwd = control.forward;
+
+        // Debug.Log(Vector3.Dot(forward, controllerfwd));
+       // Debug.Log(Vector3.Distance(forward, controllerfwd));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "cube")
+        {
+            hitCounter++;
+            Debug.Log(hitCounter);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "fullWall")
+        {
+           if((Vector3.Distance(forward, controllerfwd) > 1.0f)){
+                Debug.Log("blocked");
+
+            }
+            else
+            {
+                Debug.Log("NOT blocked");
+            }
+        }
+    }
+}
