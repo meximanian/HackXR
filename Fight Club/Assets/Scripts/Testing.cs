6dce6aaf759e594f3d471f34a2a41a1ba7e6ab11
Anthony <aostia@ucsd.edu>
2019-05-04 15:02:38 -0700

    Worked on my part

---
+++ Fight Club/Assets/Scripts/Testing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    public Transform other;
+    public GameObject player;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (other)
+        {
+            Vector3 forward = player.transform.forward;
+            Vector3 controllerfwd = other.right;
+
+            Debug.Log(Vector3.Dot(forward, controllerfwd));
+        }
+    }
+}
